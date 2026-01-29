7ff656a544ef7cdcf0d753d929e318af64fe10f1
avinashpitti <avinash87129@gmail.com>
2026-01-29 14:24:27 +0530

    basics of js

---
+++ script.js
@@ -0,0 +1,26 @@
+var eid = 101;
+
+console.log(eid)
+
+
+
+
+const a = 4;
+const b = 7;
+
+console.log(a + b);
+
+var ename = "avinash";
+
+console.log(ename)
+
+var esal = 45000.45
+console.log(esal)
+
+document.writeln(eid)
+document.writeln('<br>')
+
+document.writeln(ename);
+document.writeln('<br>')
+document.writeln(esal);
+
---
+++ three.js
@@ -0,0 +1,23 @@
+function login() {
+    console.log("login successful")
+}
+
+login()
+
+//class
+class Employee {
+
+}
+let e1 = new Employee()
+console.log(e1)
+
+//object
+var emp = {
+    eid: 102,
+    ename: "Rahul"
+}
+console.log(emp)
+
+
+
+
