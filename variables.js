59a7ac9dd227ae2cd47b30042c25a16222441e3b
avinashpitti <avinash87129@gmail.com>
2026-01-29 18:13:31 +0530

    data types

--- variables.js
+++ variables.js
@@ -17,10 +17,10 @@ console.log(ename)
 var esal = 45000.45
 console.log(esal)
 
-document.writeln(eid)
-document.writeln('<br>')
+// document.writeln(eid)
+// document.writeln('<br>')
 
-document.writeln(ename);
-document.writeln('<br>')
-document.writeln(esal);
+// document.writeln(ename);
+// document.writeln('<br>')
+// document.writeln(esal);
 
