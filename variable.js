4891543422903f96e242510b4da7c9af40706fe7
avinashpitti <avinash87129@gmail.com>
2026-01-30 18:30:26 +0530

    datatypes

---
+++ variable.js
@@ -0,0 +1,16 @@
+var colors = ['red', 'green', 'yellow', 'violet', 'indigo']
+
+console.log(colors)
+
+var sizes = [38, 40, 42, 44]
+
+var uname = 'avinash'
+
+var specifications = {
+    'name': 'avinash',
+    'id': 101,
+    'esal': 4444
+}
+
+let b = [10, 20.3, true, true, undefined]
+
