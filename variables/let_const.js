c045262bc07db6a8b7094c82a1b77cf3b26eade2
avinashpitti <avinash87129@gmail.com>
2026-02-05 23:00:24 +0530

    rearranged variables,arrays,datatypes files into respective folders

---
+++ variables/let_const.js
@@ -0,0 +1,23 @@
+let age = 21;
+age = 22; //you can reassign
+console.log(age);
+
+let user = 'avinash';
+console.log(user);
+
+// let user='pavan' // You can't accidentally declare the same variable twice
+
+const covid = 19
+// covid = 20  // variables declared with const can't be redclared or reassigned
+// const covid =20
+console.log(covid)
+
+// const num; // const variables must be assigned a value when they are declared
+// num = 10;
+const num = 10;
+console.log(num)
+
+
+
+
+
---
+++ variables/variable.js
@@ -0,0 +1,10 @@
+a = 3
+A = 4
+console.log(a)
+console.log(A)
+
+// When separated by semicolons, multiple statements on one line are allowed:
+// On the web, you might see examples without semicolons.
+// Ending statements with semicolon is not required, but highly recommended.
+x = 12; y = 13; z = 14
+console.log(x + y + z)
