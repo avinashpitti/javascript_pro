8415bbec6bb05a9333c4b3eafa8b61d353cfef88
avinashpitti <avinash87129@gmail.com>
2026-02-05 23:21:16 +0530

    variables in js

---
+++ variables/variable2.js
@@ -0,0 +1,24 @@
+let a;
+console.log(a) // undefined
+console.log(typeof (a)) // undefined
+
+let b = 12.5
+console.log(b)
+console.log(typeof (b)) // everything is a number in js
+
+let c = true;
+console.log(c)
+console.log(typeof (c))
+
+let car = ' ';
+console.log(car)
+console.log(typeof (car))
+
+const year = 2026;
+console.log(year);
+console.log(typeof year)
+
+const age = 22;
+console.log(age)
+console.log(typeof (age))
+
