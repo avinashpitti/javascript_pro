ebfb5723eb010373bb822317c08eb24d9e1a8e98
avinashpitti <avinash87129@gmail.com>
2026-02-06 17:47:03 +0530

    practice arithmetic concatenation and addition in different ways

--- operators/arithmetic.js
+++ operators/arithmetic.js
@@ -14,3 +14,39 @@ console.log(a + g)
 console.log(e + g)
 console.log(c * d)
 console.log(b * f)
+
+console.log('***********************')
+
+let num1 = 12
+let num2 = 67
+let num3 = '19'
+console.log(num1 + num2) // 79
+console.log(num1 + num2 + num3) // 7919
+// mixed values js evaluates left to right
+console.log(num3 + num1 + num2) // 191267
+
+console.log('*************Boolean values**************')
+console.log(true + true)
+console.log(true + false)
+console.log(false + false)
+
+console.log('true' + true)
+console.log(true + false + 'false')
+
+console.log('null becomes 0')
+console.log(null + 5)
+console.log(null * 5)
+console.log(null / 5)
+console.log(5 / null)
+
+console.log('undefined converts to NaN')
+console.log(undefined + 5)
+
+console.log('unary + converts values into numbers')
+console.log(+'14')//14
+console.log(+'16.5')//16.5
+
+console.log('very very important ')
+console.log(1 + '2')//12
+console.log(1 + +'2')//3
+
