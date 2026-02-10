fa46a5f2c4c2af99d88eea4cd17f1c2031adb123
avinashpitti <avinash87129@gmail.com>
2026-02-10 19:17:09 +0530

    break and continue statement using for loop

--- control_flow_statements/practice.js
+++ control_flow_statements/practice.js
@@ -1,3 +1,15 @@
+// break statement
 for (let i = 10; i > 0; i--) {
+    if (i == 5)
+        break
+
+    console.log(i)
+}
+
+// continue
+
+for (let i = 10; i > 0; i--) {
+    if (i == 5)
+        continue
     console.log(i)
 }
