53dd30f344338eb65cbf157daaf98c7118b4d892
avinashpitti <avinash87129@gmail.com>
2026-02-10 17:30:34 +0530

    do while loop

--- control_flow_statements/practice.js
+++ control_flow_statements/practice.js
@@ -1,8 +1,11 @@
-for (let i = 1; i <= 5; i++) {
+let i = 1;
+do {
     console.log(i)
-}
+    i++
+} while (i <= 7)
 
-for (let i = 20; i > 10; i--) {
-    console.log(i)
-}
 
+let num = 10;
+do {
+    console.log('Hello')
+} while (num < 5);
