f005c86541235248a09c889eb30cc283586380b4
avinashpitti <avinash87129@gmail.com>
2026-02-10 23:05:43 +0530

    practicing for of loop

--- control_flow_statements/practice.js
+++ control_flow_statements/practice.js
@@ -1,34 +1,17 @@
-// print 10 table
-for (let i = 10; i <= 100; i += 10) {
-    console.log(i)
-}
+let names = ['avi', 'rahul', 'mahesh', 'harsha', 'prabhas', 'amar']
 
-// print 11 table
-for (let i = 1; i <= 10; i++) {
-    console.log(i * 11)
-}
-
-//print 17 table
-let num = 17;
-for (let i = 1; i <= 10; i++) {
-    console.log(num * i)
-}
-
-//print 19 table
-let nums = 19;
-for (let i = 1; i <= 10; i++) {
-    console.log(nums * i)
+for (let name of names) {
+    console.log(name)
 }
 
 
-// print 12 table
-for (let i = 1; i <= 10; i++) {
-    console.log(`12 * ${i} =${12 * i}`)
+let courses = ['bca', 'bsc', 'bba', 'b.com']
+for (let course of courses) {
+    console.log(course)
 }
 
-// print 13 table
-let n = 13
-for (let i = 1; i <= 10; i++) {
-    console.log(`${n}* ${i}=${n * i}`)
+let eids = [101, 102, 103, 104, 105, 106, 107]
+for (let eid of eids) {
+    console.log(eid)
 }
 
