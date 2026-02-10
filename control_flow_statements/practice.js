5e21b921474e8e527711498230610b4198ab21d3
avinashpitti <avinash87129@gmail.com>
2026-02-10 19:42:55 +0530

    printing even and odd numbers with and without conditional using for loop

--- control_flow_statements/practice.js
+++ control_flow_statements/practice.js
@@ -1,15 +1,22 @@
-// break statement
-for (let i = 10; i > 0; i--) {
-    if (i == 5)
-        break
+// print even numbers upto 20
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0)
+        console.log(i)
+}
 
+
+// print odd numbers in between 31 and 40
+for (let i = 31; i <= 40; i++) {
+    if (i % 2 === 1)
+        console.log(i)
+}
+
+// print even numbers between 100 and 110
+for (let i = 102; i < 110; i += 2) {
     console.log(i)
 }
 
-// continue
-
-for (let i = 10; i > 0; i--) {
-    if (i == 5)
-        continue
+//print even numbers between 200 and 210
+for (let i = 201; i < 210; i += 2) {
     console.log(i)
 }
