f378ceb682efcabce7a45a1eedb95f36fbfe944f
avinashpitti <avinash87129@gmail.com>
2026-02-11 17:20:30 +0530

    add a new file mockaro.js and used for loop

--- control_flow_statements/practice.js
+++ control_flow_statements/practice.js
@@ -1,17 +1,39 @@
-let names = ['avi', 'rahul', 'mahesh', 'harsha', 'prabhas', 'amar']
 
-for (let name of names) {
-    console.log(name)
+let person = {
+    'eid': 101,
+    'ename': 'prabhas',
+    'esal': 9999,
+    'gender': 'Male'
+}
+
+// console.log(person)
+// console.log(person.ename)
+// console.log(person.eid)
+// console.log(person.gender)
+
+let emps = [
+    { eid: 11, ename: 'praba', 'gender': 'Male' },
+    { eid: 12, ename: 'avi', 'gender': 'Male' },
+    { eid: 13, ename: 'harsha', 'gender': 'Male' }
+
+]
+
+for (let emp of emps) {
+    console.log(emp.ename)
+}
+
+// print even numbers
+let nums = [2, 5, 78, 34, 212, 45]
+
+for (let num of nums) {
+    if (num % 2 === 0) (
+        console.log(num)
+    )
 }
 
 
-let courses = ['bca', 'bsc', 'bba', 'b.com']
-for (let course of courses) {
-    console.log(course)
-}
 
-let eids = [101, 102, 103, 104, 105, 106, 107]
-for (let eid of eids) {
-    console.log(eid)
-}
+
+
+
 
